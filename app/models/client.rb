db921008b3656a68621d8a5db9b44b5d01759c20
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-09-25 00:46:18 +0200

    add short name function

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ActiveRecord::Base
   has_many :orders
   validates :first_name, :last_name, :city, :street, :zipcode, :country,
     presence: true
+
+  def short_name
+    "#{first_name[0]}. #{last_name}"
+  end
 end
