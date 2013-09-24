470674ff98ce894b5bacd88fa07d204d49d5cd20
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-09-25 00:44:49 +0200

    finalize order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,6 +69,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:content, :driver_id, :company_id)
+      params.require(:order).permit(:content, :driver_id, :company_id, :client_id)
     end
 end
