d7dd3ec6cb62e725f3bfe778be00739412aa7fae
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-10-11 15:06:24 +0200

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,17 +44,25 @@ end
 
 sample_headquarter
 
-company1 = sample_company(name: "Rialto Göttingen GmbH", zipcode: "37073", street: "Weender Landstraße 3")
-company2 = sample_company(name: "Joey's Pizza Göttingen Nord", zipcode: "37075", street: "An der Lutter 22")
-sample_company(name: "Call a Pizza Göttingen Nord", zipcode: "37075", street: "Weender Landstraße 77")
-sample_company(name: "Joey's Pizza Göttingen Süd", zipcode: "37078", street: "Geismar Landstraße 36")
+company1 = sample_company(name: "Risotto Göttingen GmbH", zipcode: "37073", street: "Weender Landstraße 3")
+company2 = sample_company(name: "Jenny's Pizza Göttingen Nord", zipcode: "37075", street: "An der Lutter 22")
+sample_company(name: "Eat a Pizza Göttingen Nord", zipcode: "37075", street: "Weender Landstraße 77")
+sample_company(name: "Jenny's Pizza Göttingen Süd", zipcode: "37078", street: "Geismar Landstraße 36")
 
 client1 = sample_client(first_name: "Hendrik", street: "Kreuzbergring 4", zipcode: 37075)
 client2 = sample_client(first_name: "Bene", street: "Kreuzbergring 28", zipcode: 37075)
+client3 = sample_client(first_name: "Sarah", street: "Zimmermannstraße 5", zipcode: 37075)
+client4 = sample_client(first_name: "Torkonen", street: "Waldweg 2", zipcode: 37075)
+client5 = sample_client(first_name: "Wolverine", street: "Platz der Göttinger Sieben 5", zipcode: 37075)
+client6 = sample_client(first_name: "Storm", street: "Philipp Reiss Straße 1", zipcode: 37075)
 
 sample_driver(name: "Luigi", street: "Von Ossietzky Str. 1",  zipcode: 37085)
 sample_driver(name: "Mario", street: "Göttinger Str. 41",  zipcode: 37124)
 sample_driver(name: "Bowser", street: "Kurze-Geismar-Str. 41",  zipcode: 37073)
 
 client1.orders.create(company: company1, content: "1 * Suflaki", capacity: rand(Order::CAPACITY))
-client2.orders.create(company: company2, content: "5 * Suflaki", capacity: rand(Order::CAPACITY))
+client2.orders.create(company: company2, content: "2 * Pizzabrötchen", capacity: rand(Order::CAPACITY))
+client3.orders.create(company: company1, content: "1 * Suflaki", capacity: rand(Order::CAPACITY))
+client4.orders.create(company: company2, content: "2 * Pizzabrötchen", capacity: rand(Order::CAPACITY))
+client5.orders.create(company: company2, content: "2 * Sucuk", capacity: rand(Order::CAPACITY))
+client6.orders.create(company: company2, content: "2 * Salami", capacity: rand(Order::CAPACITY))
